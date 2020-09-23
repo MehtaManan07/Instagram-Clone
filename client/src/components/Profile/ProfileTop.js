5a571d11493745d799ba6d87ade19e9c576d865f
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-09-23 15:04:35 +0530

    profile photo styling

--- client/src/components/Profile/ProfileTop.js
+++ client/src/components/Profile/ProfileTop.js
@@ -7,7 +7,7 @@ const ProfileTop = ({ user, loading }) => {
 
       <header className="profile__header">
         <div className="profile__column">
-          <img src='/images/avatar.jpg' alt='profile pic' />
+          <img src={user.photo} alt='profile pic' />
         </div>
         <div className="profile__column">
           <div className="profile__title">
