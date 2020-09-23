3fc42925461091a04ad7c73bfac30fb19d2b6a1f
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-09-23 10:21:00 +0530

    uploading the redux state for logged in user

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Login from './pages/Auth/Login';
 import Signup from './pages/Auth/Signup';
+import { useDispatch } from 'react-redux';
+import { getUser } from './redux/actions/authActions';
 
 function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    if(localStorage.getItem('Instagram1')){
+      dispatch(getUser())
+    }
+  },[])
   return (
     <>
     {/* <Navbar /> */}
--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -5,6 +5,10 @@ import {
   SIGNUP_FAILURE,
   SIGNUP_SUCCESS,
   SIGNUP_REQUEST,
+  LOGOUT_SUCCESS,
+  LOGOUT_FAILURE,
+  GET_ME_SUCCESS,
+  GET_ME_FAILURE
 } from '../types';
 import axios from 'axios';
 
@@ -34,6 +38,37 @@ export const loginUser = (userData) => async (dispatch) => {
     dispatch({
       type: LOGIN_FAILURE,
       payload: displayErr,
+    });
+  }
+};
+
+export const getUser = () => async (dispatch) => {
+  try {
+    const { data } = await axios.get(`/api/v1/users/me`);
+    dispatch({
+      type: GET_ME_SUCCESS,
+      payload: data.data,
+    });
+
+    console.log(data);
+  } catch (error) {
+    dispatch({
+      type: GET_ME_FAILURE,
+      payload: error.response.data.error,
+    });
+  }
+};
+
+export const logoutUser = () => async (dispatch) => {
+  try {
+    await axios.get(`/api/v1/users/logout`);
+    dispatch({
+      type: LOGOUT_SUCCESS,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGOUT_FAILURE,
+      payload: error.response.data.error,
     });
   }
 };
