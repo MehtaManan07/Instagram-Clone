0d685c9d6823d63fb34082c71a2098a154a0a511
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-09-24 22:05:18 +0530

    local state on new post

--- client/src/components/Post/PostForm.js
+++ client/src/components/Post/PostForm.js
@@ -1,58 +1,54 @@
 import React from 'react';
 
-const PostForm = () => {
+const PostForm = ({ values, setValues, submitHandler }) => {
+  const onChangeHandler = (name) => (e) => {
+    setValues({ ...values, [name]: e.target.value });
+  };
+
+  const imageChangeHandler = (e) => {
+    console.log(e.target.files[0])
+    setValues({ ...values, photo: e.target.files[0] });
+    console.log(values);
+  };
+
   return (
-    <div id="edit-profile">
-      <form action="" className="edit-profile__form">
-        <div className="form__row">
-          <label for="full-name" className="form__label">
-            Name:
-          </label>
-          <input id="full-name" type="text" className="form__input" />
-        </div>
-        <div className="form__row">
-          <label for="user-name" className="form__label">
-            Username:
-          </label>
-          <input id="user-name" type="text" className="form__input" />
-        </div>
-        <div className="form__row">
-          <label for="website" className="form__label">
-            Website:
-          </label>
-          <input id="website" type="url" className="form__input" />
-        </div>
-        <div className="form__row">
-          <label for="bio" className="form__label">
-            Bio:
-          </label>
-          <textarea id="bio"></textarea>
-        </div>
-        <div className="form__row">
-          <label for="email" className="form__label">
-            Email:
-          </label>
-          <input id="email" type="email" className="form__input" />
-        </div>
-        <div className="form__row">
-          <label for="phone" className="form__label">
-            Phone Number:
-          </label>
-          <input id="phone" type="tel" className="form__input" />
-        </div>
-        <div className="form__row">
-          <label for="gender" className="form__label">
-            Gender:
-          </label>
-          <select id="gender">
-            <option value="Male">Male</option>
-            <option value="Female">Female</option>
-            <option value="Other">Can't remember</option>
-          </select>
-        </div>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
+    <form action="" className="edit-profile__form">
+      <div className="form__row">
+        <label className="form__label">Name:</label>
+        <input
+          type="text"
+          value={values.name}
+          onChange={onChangeHandler('name')}
+          className="form__input"
+        />
+      </div>
+      <div className="form__row">
+        <label className="form__label">Description:</label>
+        <textarea
+          type="text"
+          value={values.description}
+          onChange={onChangeHandler('description')}
+          className="form__input"
+        />
+      </div>
+      <div className="form__row">
+        <label className="form__label">Image:</label>
+        <input
+          type="file"
+          onChange={imageChangeHandler}
+          className="form__input"
+        />
+      </div>
+      <div className="text-center">
+        <span
+          style={{ width: '255px' }}
+          className="btn btn-lg btn-outline-primary"
+          onClick={submitHandler}
+        >
+          Submit
+        </span>
+      </div>
+    </form>
   );
 };
 
--- client/src/pages/Post/NewPost.js
+++ client/src/pages/Post/NewPost.js
@@ -1,66 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PostForm from '../../components/Post/PostForm';
 import './Edit.css';
 
 const NewPost = () => {
+  const [values, setValues] = useState({
+    name: '',
+    description: '',
+    photo: '',
+    url: ''
+  });
+
+  const uploadPhoto = () => {};
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    uploadPhoto();
+    console.log(values);
+  };
   return (
     <main id="edit-profile">
-      <div class="edit-profile__container">
-        <header class="edit-profile__header">
-          <div class="edit-profile__avatar-container">
-            <img src="images/avatar.jpg" class="edit-profile__avatar" />
+      <div className="edit-profile__container">
+        <header className="edit-profile__header">
+          <div className="edit-profile__avatar-container">
+            <img
+              src="https://cdn.pixabay.com/photo/2017/11/10/05/04/instagram-2935404__340.png"
+              alt='logo'
+              className="edit-profile__avatar"
+            />
           </div>
-          <h4 class="edit-profile__username">serranoarevalo</h4>
+          <h3 className="edit-profile__username">New Post</h3>
         </header>
-        <form action="" class="edit-profile__form">
-          <div class="form__row">
-            <label for="full-name" class="form__label">
-              Name:
-            </label>
-            <input id="full-name" type="text" class="form__input" />
-          </div>
-          <div class="form__row">
-            <label for="user-name" class="form__label">
-              Username:
-            </label>
-            <input id="user-name" type="text" class="form__input" />
-          </div>
-          <div class="form__row">
-            <label for="website" class="form__label">
-              Website:
-            </label>
-            <input id="website" type="url" class="form__input" />
-          </div>
-          <div class="form__row">
-            <label for="bio" class="form__label">
-              Bio:
-            </label>
-            <textarea id="bio"></textarea>
-          </div>
-          <div class="form__row">
-            <label for="email" class="form__label">
-              Email:
-            </label>
-            <input id="email" type="email" class="form__input" />
-          </div>
-          <div class="form__row">
-            <label for="phone" class="form__label">
-              Phone Number:
-            </label>
-            <input id="phone" type="tel" class="form__input" />
-          </div>
-          <div class="form__row">
-            <label for="gender" class="form__label">
-              Gender:
-            </label>
-            <select id="gender">
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-              <option value="cant">Can't remember</option>
-            </select>
-          </div>
-          <input type="submit" value="Submit" />
-        </form>
+        <PostForm
+          submitHandler={submitHandler}
+          values={values}
+          setValues={setValues}
+        />
       </div>
     </main>
   );
