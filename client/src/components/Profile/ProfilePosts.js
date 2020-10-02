81b44596eea418115fedc59ac23007de9bb7718f
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-10-02 22:56:09 +0530

    no post message and console warnings

--- client/src/components/Profile/ProfilePosts.js
+++ client/src/components/Profile/ProfilePosts.js
@@ -1,28 +1,32 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom'
+import { useHistory } from 'react-router-dom';
 const ProfilePost = ({ posts }) => {
-  const history = useHistory()
+  const history = useHistory();
   return (
     <>
       <section className="profile__photos">
-        {posts.map((post) => (
-          <>
-            <div onClick={() => history.push(`/post/${post._id}`)} className="profile__photo">
-              <img
-                src={post.image}
-              />
-              <div className="profile__photo-overlay">
-                <span className="overlay__item">
-                  <i className="fa fa-heart"></i>
-                  {post.likes.length}
-                </span>
-                <span className="overlay__item">
-                  <i className="fa fa-comment"></i>
-                  {post.comments.length}
-                </span>
+        {posts.length < 1 ? (
+          <h3 style={{ margin: '0 auto' }}> No posts yet </h3>
+        ) : (
+          posts.map((post) => (
+            <>
+              <div
+                onClick={() => history.push(`/post/${post._id}`)}
+                className="profile__photo"
+              >
+                <img src={post.image} alt='post' />
+                <div className="profile__photo-overlay">
+                  <span className="overlay__item">
+                    <i className="fa fa-heart"></i>
+                    {post.likes.length}
+                  </span>
+                  <span className="overlay__item">
+                    <i className="fa fa-comment"></i>
+                    {post.comments.length}
+                  </span>
+                </div>
               </div>
-            </div>
-            {/* <div className="profile__photo">
+              {/* <div className="profile__photo">
         <img src="/images/feedPhoto.jpg" />
         <div className="profile__photo-overlay">
           <span className="overlay__item">
@@ -61,8 +65,9 @@ const ProfilePost = ({ posts }) => {
           </span>
         </div>
       </div> */}
-          </>
-        ))}
+            </>
+          ))
+        )}
       </section>
     </>
   );
